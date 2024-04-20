c1145aecf0cf348e81ce03775f7b2a2fca877a32
hardiksaraiya2203 <hardiksaraiya2203@gmail.com>
2024-04-20 14:46:55 +0530

    vue changes

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>hello {{Fname}} {{Mname}}</div>
+  <div v-text="Lname"></div>
+  <!-- error v-text will override the children   -->
+  <p v-html="Price"></p>
+  <!-- do not bind api data which causes easily vurnarable to hackers  -->
+
+  <!-- binding attribute bool and id  -->
+  <h1 v-bind:id="headerId">Heading</h1>
+  <button v-bind:disabled="isDisabled">Bind</button>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      Fname: "Hardik",
+      Lname: "Saraiya",
+      Mname: "Umesh",
+      Price: `<a href="#" onclick="alert('you are hacked')">Win a Price</a>`,
+      headerId : "headerId",
+      isDisabled : "true",
+    };
+  },
+};
 </script>
 
 <style>
